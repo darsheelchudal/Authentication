df7b3080db8b2ce9d89c11be94eb4d03fbc7d908
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-12 13:30:50 +0545

    authUser

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -3,7 +3,23 @@ import User from "../models/users.model.js";
 import generateToken from "../utils/generateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
