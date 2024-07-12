4effdf8d22ee138bc53c705f23b2cd6a042c7634
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-12 12:45:38 +0545

    JWT token signed to the httpOnly cookie

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/users.model.js";
+import generateToken from "../utils/generateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
@@ -16,6 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const user = await User.create({ name, email, password });
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,16 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_KEY, {
     expiresIn: "30d",
   });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
 };
+
+export default generateToken;
