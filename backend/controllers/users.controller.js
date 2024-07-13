c646bee5b50093bd5a0c372e491e287699354af4
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-13 13:33:39 +0545

    backend api completed

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -26,10 +26,7 @@ const authUser = asyncHandler(async (req, res) => {
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  console.log("Request body", req.body);
-
   const userExists = await User.findOne({ email });
-  console.log(userExists);
 
   if (userExists) {
     res.status(400);
@@ -65,10 +62,38 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get User profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
+
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update User Profile" });
+  console.log("Request body", req.body);
+
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      password: updatedUser.password,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,18 +7,13 @@ dotenv.config();
 
 const protect = () =>
   asyncHandler(async (req, res, next) => {
-    console.log("Protect middleware executed");
     const token = req.cookies.jwt;
-    console.log("Token:", token);
     if (token) {
       try {
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-        console.log("Decoded:", decoded);
         req.user = await User.findById(decoded.userId).select("-password");
-        console.log("User:", req.user);
         next();
       } catch (err) {
-        console.log("Error:", err);
         res.status(401);
         throw new Error("Not authorized, invalid token");
       }
